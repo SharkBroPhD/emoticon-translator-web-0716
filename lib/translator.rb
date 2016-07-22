857ba5ea7579f06f0897bdc6ba926bcaa59e70c0
SharkBroPhD <troychryssos@gmail.com>
2016-07-22 19:46:26 -0400

    Wrote methods to translate emoticons

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,46 @@
-# require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(file_path)
+  emoticons=YAML.load_file(file_path)
+  
+  actions={"get_meaning"=>{},"get_emoticon"=>{}}
+
+  emoticons.each do |meaning, emote_array|
+    actions["get_meaning"][emote_array[1]]=meaning
+    actions["get_emoticon"][emote_array[0]]=emote_array[1]
+  end
+  actions
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file_path,emoticon)
+  actions=load_library(file_path)
+  def_found=false
+  actions.each do |library,content|
+    content.each do |words, definitions|
+      if words==emoticon
+        return definitions
+        def_found=true
+      end
+    end
+  end
+  if def_found==false
+    return "Sorry, that emoticon was not found"
+  end
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file_path,emoticon)
+  actions=load_library(file_path)
+  def_found=false
+  actions.each do |library,content|
+    content.each do |words, definitions|
+      if words==emoticon
+        return definitions
+        def_found=true
+      end
+    end
+  end
+  if def_found==false
+    return "Sorry, that emoticon was not found"
+  end
 end
